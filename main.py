02c010ad1e5c63c9a95979f209e4c6543f7df860
Hsingyu120 <b05611012@ntu.edu.tw>
2022-08-21 00:24:59 +0800

    delete files not in use

--- main.py
+++
@@ -1,6 +0,0 @@
-import housing
-
-price1 = housing.Price("c:151","Taiwan")
-price2 = housing.Price("c:125","TPE_YoY")
-price1.df.plot('Date','Index',title=f"Source: {price1.source} Price Index: {price1.target}",legend=False)
-price2.df.plot('Date','Index',title=f"Source: {price2.source} Price Index: {price2.target}",legend=False)
